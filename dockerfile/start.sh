e85023536a38c6cc735bbc7d90a5e3ddda4959a8
Phuong Le <tungphuongld@gmail.com>
2015-06-04 13:55:15 +0700

    no message

--- dockerfile/start.sh
+++ dockerfile/start.sh
@@ -1,7 +1,7 @@
 cd /tmp
 rm -rf nodeTutorial; true
 git clone https://github.com/jackyphuong/nodeTutorial.git
-cd /nodeTutorial/rethinkdb
-npm install
+#cd /nodeTutorial/rethinkdb
+#npm install
 
-node .
+#node .
